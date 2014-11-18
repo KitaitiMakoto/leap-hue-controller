038880d637a76be44c9d5dec58d82531afa6abe3
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-11-18 19:20:28 +0900

    Hueのリクエスト後もtransition duration分はLeapを無視する。この方がリクエストが溜まらず応答性がよい

--- app/lighter.rb
+++ app/lighter.rb
@@ -14,13 +14,14 @@ class Lighter
 
   def light(hue:, saturation: nil, brightness: nil)
     @transiting = true
+    EM.add_timer @transition_duration do
+      @transiting = false
+    end
     params = {hue: hue}
     params[:sat] = saturation if saturation
     params[:bri] = brightness if brightness
     $stderr.puts params.inspect if $DEBUG or $VERBOSE
-    @light.set_state_async(params, (@transition_duration * 10).round) do
-      @transiting = false
-    end
+    @light.set_state_async(params, (@transition_duration * 10).round)
   end
 end
 
